fcfc4c26a7d34c3076a2d04588e6fb9b16b1a61c
Xin Liu <37545515+liuxin2361@users.noreply.github.com>
2024-05-18 13:22:45 +0900

    fetch data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,21 @@
-export default function Page() {
-    return <p>Dashboard</p>
+import { lusitana } from '@/app/ui/fonts';
+import { fetchRevenue, fetchLatestInvoices } from '@/app/lib/data';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+
+export default async function Page() {
+    const revenue = await fetchRevenue();
+    const latestInvoices = await fetchLatestInvoices();
+    return (
+        <main>
+            <h1 className={'${lusitana.className}} mb-4 text-x1 md:text-2x1'}>
+                Dashboard
+            </h1>
+            <div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-4'></div>
+            <div className='mt-6 grid grid-cols-1 gap-6 sm:grid-cols-4 lg:grid-cols-8'>
+                {<RevenueChart revenue={revenue} />}
+                {<LatestInvoices latestInvoices={latestInvoices} />}
+            </div>
+        </main>
+    )
 }
