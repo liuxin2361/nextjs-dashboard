6818e7167fc6079d03f2de3190a5313afd253e7b
Xin Liu <37545515+liuxin2361@users.noreply.github.com>
2024-05-18 21:57:01 +0900

    add Card Compent

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -3,6 +3,7 @@ import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 import { LatestInvoice } from '@/app/lib/definitions';
+
 export default async function LatestInvoices({
   latestInvoices,
 }: {
