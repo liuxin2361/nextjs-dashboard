705377a40953b4b8efa35873c807fbdb24be24a2
Xin Liu <37545515+liuxin2361@users.noreply.github.com>
2024-05-28 22:46:35 +0900

    Adding Customer Page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,22 @@
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
+import { lusitana } from "@/app/ui/fonts";
+import { CreateCustomer } from "@/app/ui/invoices/buttons";
+import Search from "@/app/ui/search";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
     title: 'Customers',
 };
 
-export default function Page() {
-    return <p>Customers Page</p>
+export default async function Page({ searchParams, }: { searchParams?: { query?: string, page?: string }; }) {
+    const query = searchParams?.query || '';
+    const customers = await fetchFilteredCustomers(query);;
+
+    return (
+        <Suspense>
+            <CustomersTable customers={customers} />
+        </Suspense>
+    );
 }
